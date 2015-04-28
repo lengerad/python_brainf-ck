2c76a73eafbf7fe621e543d82573252add601a4a
lengerad <lengerad@fit.cvut.cz>
2015-04-28 17:00:02 +0200

    Priprava filteru

--- myPNGlibrary.py
+++ myPNGlibrary.py
@@ -19,6 +19,9 @@ class pngHandler():
 
         # print(self.binaryData)
         self.createArray()
+        self.a = (0, 0, 0)
+        self.b = (0, 0, 0)
+        self.c = (0, 0, 0)
 
 
     def checkHeader(self, binaryData):
@@ -60,20 +63,39 @@ class pngHandler():
             if chunk['chunkType'] == b'IDAT':
                 self.decompressedData += zlib.decompress(chunk['chunkData'])
 
-    def filterMeDude(self, filterType, pixel):
+    def reconstructPixel(self, filterType, pixel):
         if filterType == 0:
             return pixel
         if filterType == 1:
-            pass
+            return self.sumBytes(pixel, self.a)
         if filterType == 2:
-            pass
-
+            return self.sumBytes(pixel, self.b)
         if filterType == 3:
-            pass
-
+            return self.sumBytes(pixel, self.sumBytes(self.a, self.b) / 2)
         if filterType == 4:
-            pass
+            return self.sumBytes(pixel, self.paethPredictor(self.a, self.b, self.c))
 
+    def sumBytes(self, pixel, pixel2):
+        return ((pixel[0] + pixel2[0]) % 256, (pixel[1] + pixel2[1]) % 256, (pixel[2] + pixel2[2]) % 256)
+
+
+    def paethPredictor(self, pixelA, pixelB, pixelC):
+        newA = self.paeth(pixelA[0], pixelB[0], pixelC[0])
+        newB = self.paeth(pixelA[1], pixelB[1], pixelC[1])
+        newC = self.paeth(pixelA[2], pixelB[2], pixelC[2])
+        return (newA, newB, newC)
+
+    def paeth(self, a, b, c):
+        p = a + b - c
+        pa = abs(p - a)
+        pb = abs(p - b)
+        pc = abs(p - c)
+        if pa <= pb and pa <= pc:
+            return a
+        elif pb <= pc:
+            return b
+        else:
+            return c
 
     def createArray(self):
         self.checkHeader(self.binaryData)
@@ -93,7 +115,7 @@ class pngHandler():
             for j in range(0, self.imageLength):
                 pixel = (
                 self.decompressedData[pointer], self.decompressedData[pointer + 1], self.decompressedData[pointer + 2])
-                self.pictureArray[i][j] = self.filterMeDude(filter, pixel)
+                self.pictureArray[i][j] = self.reconstructPixel(filter, pixel)
                 pointer += 3
 
         print(self.pictureArray)
